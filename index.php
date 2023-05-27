8e816427a726fc421317b045d680bc16018ca35e
HR-Riad <hasanur15-12804@diu.edu.bd>
2023-05-27 23:22:05 +0600

    first code

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<?php 
+
+echo " php rocks ";
+
+echo "<br>";
+
+echo "27/05/2023";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title> <?php echo "Page Title"  ?> </title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p> <?php echo "This is a paragraph." ?></p>
+
+</body>
+</html>
+
+<?php
+
+echo 12+17-22;
+
+?>
+
+
